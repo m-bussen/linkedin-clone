ab342b35cafd923330c938cfe7cec7af61900089
mbussen <busfactorx@gmail.com>
2021-06-12 20:32:11 -0400

    user photo authentication working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Login from './components/Login';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { getUserAuth } from "./actions";
+import { connect } from "react-redux";
 
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
 
-function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <Login />
           </Route>
-          <Route path='/home'>
+          <Route path="/home">
             <Header />
             <Home />
           </Route>
@@ -23,4 +29,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
